d801e13e54a0c79b7bb4a6c8575f5e0b1db80af0
bwkw <na.shota.1139@gmail.com>
2023-05-04 06:54:09 -0700

    :sparkles: Add submit roulette result function

--- pages/mtb.tsx
+++ pages/mtb.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import type { RouletteOption } from '@/components/Roulette'
 import { Roulette } from '@/components/Roulette'
 
+// Rouletteコンポーネントのdataの型の都合上、option: string
 const optionA: RouletteOption[] = [
   { id: 1, option: '10' },
   { id: 2, option: '-30' },
@@ -16,8 +17,12 @@ const optionB: RouletteOption[] = [
 ]
 
 export default function Mtb() {
-  const [top, setTop] = useState('')
-  const [bottom, setBottom] = useState('')
+  const [top, setTop] = useState('開始待ち')
+  const [bottom, setBottom] = useState('開始待ち')
+
+  const submitRouletteResult = () => {
+    console.log(top, bottom)
+  }
 
   return (
     <>
@@ -27,6 +32,14 @@ export default function Mtb() {
           <Roulette options={optionA} value={top} setValue={setTop} />
           <Roulette options={optionB} value={bottom} setValue={setBottom} />
         </div>
+        <button
+          className='mt-10 rounded-md bg-indigo-600 px-10 py-2 font-bold text-white shadow-md transition duration-200 ease-in hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 active:bg-indigo-800'
+          onClick={() => {
+            submitRouletteResult()
+          }}
+        >
+          ルーレットの結果を反映
+        </button>
       </div>
     </>
   )
