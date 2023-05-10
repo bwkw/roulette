234391f443fa0896d7827c2ab036eda4de60821b
bwkw <na.shota.1139@gmail.com>
2023-05-10 07:52:35 -0700

    :fire: Remove max and min

--- components/RoulettePointInput.tsx
+++ components/RoulettePointInput.tsx
@@ -1,33 +1,24 @@
 import type { FC } from 'react'
 
 type RoulettePointInputProps = {
-  min: number
-  max: number
   point: number
   setPoint: (value: number) => void
 }
 
-export const RoulettePointInput: FC<RoulettePointInputProps> = ({ min, max, point, setPoint }) => {
+export const RoulettePointInput: FC<RoulettePointInputProps> = ({ point, setPoint }) => {
   const handlePointsChange = (inputValue: number) => {
-    if (inputValue < min) {
-      setPoint(min)
-    } else if (inputValue > max) {
-      setPoint(max)
-    } else if (!Number.isInteger(inputValue)) {
+    if (!Number.isInteger(inputValue)) {
       setPoint(Math.round(inputValue))
     } else {
       setPoint(inputValue)
     }
   }
 
-  console.log(point)
   return (
     <div className='flex items-center'>
       <input
         type='number'
         value={point === 0 ? '' : point}
-        min={min}
-        max={max}
         onChange={(e) => {
           handlePointsChange(Number(e.target.value))
         }}
@@ -43,7 +34,7 @@ export const RoulettePointInput: FC<RoulettePointInputProps> = ({ min, max, poin
         }}
         className='rounded border border-gray-300 px-3 py-2 text-3xl text-black focus:border-blue-300 focus:outline-none'
       />
-      <div className='text-black'>pt</div>
+      <div className='text-2xl text-black'>pt</div>
     </div>
   )
 }
